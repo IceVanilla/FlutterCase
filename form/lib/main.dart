e37f8a17a05bdef0eaf2e70b5351ea99a6500ee8
IceVanilla <912575563@qq.com>
2021-10-18 09:29:17 +0800

    form: empty page
    标题为Form的空白页面

--- form/lib/main.dart
+++ form/lib/main.dart
@@ -7,13 +7,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+      home: FormPage(),
+    );
+  }
+}
+
+class FormPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => FormState();
+}
+
+class FormState extends State<FormPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Form"),
       ),
     );
   }
