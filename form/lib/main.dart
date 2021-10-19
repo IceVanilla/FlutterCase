b2303997e40472ceb0d6ecb83c4d2a1f134309de
IceVanilla <912575563@qq.com>
2021-10-19 16:58:27 +0800

    form: permission
    添加选择权限的复选框

--- form/lib/main.dart
+++ form/lib/main.dart
@@ -21,6 +21,7 @@ class FormState extends State<FormPage> {
   String _phone_type = "mobile";
   bool _notification = false;
   double _volume = 50;
+  bool _storage = true, _location = false;
 
   @override
   Widget build(BuildContext context) {
@@ -147,7 +148,36 @@ class FormState extends State<FormPage> {
                         });
                       },
                     ),
-                  )
+                  ),
+                  ListTile(
+                    title: Text("Permission"),
+                    subtitle: Column(
+                      children: <Widget>[
+                        ListTile(
+                          title: Text("Storage"),
+                          leading: Checkbox(
+                            value: _storage,
+                            onChanged: (bool? value) {
+                              setState(() {
+                                _storage = value as bool;
+                              });
+                            }
+                          )
+                        ),
+                        ListTile(
+                          title: Text("Location"),
+                          leading: Checkbox(
+                            value: _location,
+                            onChanged: (bool? value) {
+                              setState(() {
+                                _location = value as bool;
+                              });
+                            }
+                          )
+                        )
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
