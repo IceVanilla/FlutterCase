8e12f9dc5ce10c741b992d5d63b06fda256206bf
IceVanilla <912575563@qq.com>
2021-10-19 15:31:57 +0800

    form: tabs
    添加Tab页面及切换

--- form/lib/main.dart
+++ form/lib/main.dart
@@ -20,9 +20,39 @@ class FormPage extends StatefulWidget {
 class FormState extends State<FormPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Form"),
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('TabBar Widget'),
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(
+                child: Icon(
+                  Icons.face,
+                  color: Colors.white,
+                ),
+              ),
+              Tab(
+                child: Icon(
+                  Icons.settings,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: <Widget>[
+            Center(
+              child: Text("Information"),
+            ),
+            Center(
+              child: Text("Setting"),
+            ),
+          ],
+        ),
       ),
     );
   }
