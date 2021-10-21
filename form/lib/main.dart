2e1ce4b40da625447979dd2cce74de152866590d
IceVanilla <912575563@qq.com>
2021-10-21 17:33:44 +0800

    form: tabs

--- form/lib/main.dart
+++ form/lib/main.dart
@@ -20,9 +20,41 @@ class FormPage extends StatefulWidget {
 class FormState extends State<FormPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Form"),
+    return DefaultTabController(
+      initialIndex: 0,
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('TabBar Widget'),
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(
+                child: Icon(
+                  Icons.face,
+                  color: Colors.white,
+                ),
+              ),
+              Tab(
+                child: Icon(
+                  Icons.settings,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            SingleChildScrollView(
+              padding: EdgeInsets.all(10),
+              child: Text("Information"),
+            ),
+            SingleChildScrollView(
+              padding: EdgeInsets.all(10),
+              child: Text("Setting"),
+            ),
+          ],
+        ),
       ),
     );
   }
